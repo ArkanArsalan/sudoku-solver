c62ab716766e51d9567f9b1b2d60830e3adaf2d0
Arkan Arsalan <arkan.arsalan.amanullah@gmail.com>
2023-11-06 02:38:41 +0700

    Change algorithm

--- sudoku_solver2.py
+++ sudoku_solver2.py
@@ -1,83 +1,145 @@
+import copy
 
 class SudokuSolver2():
     def __init__(self, board):
         self.board = board
-        self.empty_cell = self.find_empty()
 
-    def find_empty(self):          
-        empty_cell = []
+    def get_empty_cells(self):
+        # Initialize list to store empty cells
+        empty_cells = []
+
+        # Iterate for every cell, if the cell contain 0 add to the empty cells list
         for i in range(len(self.board)):
             for j in range(len(self.board)):
                 if self.board[i][j] == 0:
-                    empty_cell.append((i,j))
-        return empty_cell
-    
-    def check_validation(self, num, pos):
-        # Check row
-        for i in range(len(self.board)):
-            if self.board[pos[0]][i] == num:
-                return False
+                    empty_cells.append((i, j))
 
-        # check column
+        return empty_cells
+
+    def get_possible_values(self, row, col):
+        # List of all values that can be inserted to the cells
+        possible_values = [1,2,3,4,5,6,7,8,9]
+
+        # Iterate every column check if number from 1-9 already exist or not
+        # If it exist, remove from possible value list
+        for j in range(len(self.board)):
+            if self.board[row][j] in possible_values:
+                possible_values.remove(self.board[row][j])
+
+        # Iterate every row check if number from 1-9 already exist or not
+        # If it exist, remove from possible value list
         for i in range(len(self.board)):
-            if self.board[i][pos[1]] == num:
-                return False
-        
-        # Check box
-        box_col = pos[1] // 3
-        box_row = pos[0] // 3
-        
+            if self.board[i][col] in possible_values:
+                possible_values.remove(self.board[i][col])
+
+        # Iteraite every number in a the box, check if number from 1-9 already exist or not
+        # If it exist, remove from possible value list
+        box_col = col // 3
+        box_row = row // 3
 
         for i in range(box_row * 3, box_row * 3 + 3):
             for j in range(box_col * 3, box_col * 3 + 3):
-                if self.board[i][j] == num and (i,j) != pos:
-                    return False
-                
-        return True
-    
-    def forward_checking(self):                   
-        domains = {}
-        for pos in self.empty_cell:
-            domains[pos] = []
-            for i in range(1, 10):
-                if self.check_validation(i, pos):
-                    domains[pos].append(i)
+                if self.board[i][j] in possible_values:
+                    possible_values.remove(self.board[i][j])
 
-            if domains[pos] == []:
-                domains[pos].pop
+        # Return list of possible value can be inserted in current cells
+        return list(possible_values)
 
-        return domains
-
-    def mrv(self, domains):              
-        position = []
-        max_possible_input = 10   
-        for domain in domains:
-            if len(domains[domain]) == max_possible_input:
-                position.append(domain)
-            elif len(domains[domain]) < max_possible_input:
-                position.clear()
-                position.append(domain)
-                max_possible_input = len(domains[domain])
+    def forward_checking(self, empty_cells, n_domain):
+        # Initialize new domain
+        new_domain = copy.deepcopy(n_domain)
         
-        return position.pop()
-    
-    
-    def solve(self):              
-        domains = self.forward_checking()
-        if not domains:
+        # Iterate every cell in empty cells list
+        for cell in empty_cells:
+
+            row, col = cell
+            possible_values = new_domain[(row, col)]
+
+            # Iterate every column check if number from 1-9 already exist or not
+            # If it exist, remove from possible value list
+            for j in range(len(self.board)):
+                if self.board[row][j] in possible_values:
+                    possible_values.remove(self.board[row][j])
+
+            # Iterate every row check if number from 1-9 already exist or not
+            # If it exist, remove from possible value list
+            for i in range(len(self.board)):
+                if self.board[i][col] in possible_values:
+                    possible_values.remove(self.board[i][col])
+
+            # Iterate every number in the box, check if number from 1-9 already exist or not
+            # If it exist, remove from possible value list
+            box_row = (row // 3) * 3
+            box_col = (col // 3) * 3
+
+            for i in range(box_row, box_row + 3):
+                for j in range(box_col, box_col + 3):
+                    if self.board[i][j] in possible_values:
+                        possible_values.remove(self.board[i][j])
+
+            new_domain[(row, col)] = possible_values
+            
+        return new_domain
+
+    # Choosing cell with the least amouont of possible values
+    def mrv(self, empty_cells, domain):
+        minimum_cell = None
+        minimum_values = float('inf')
+
+        # Iterate for every cell in empty cells list
+        # Find the cell with the lest amount of possible values
+        for cell in empty_cells:
+            values = domain[cell]
+            if len(values) < minimum_values:
+                minimum_cell = cell
+                minimum_values = len(values)
+
+        return minimum_cell
+
+    def solve(self):
+        # Get all empty cells in sudoku
+        empty_cells = self.get_empty_cells()
+
+        # Domain of all possible values in the empty cells
+        domain = {}
+        
+        # Iterate over empty cell and find all possible values that can be inserted to the cell
+        for cell in empty_cells:
+            domain[cell] = self.get_possible_values(cell[0], cell[1])
+
+        # Solve the sudoku
+        self._solve(empty_cells, domain)
+
+    def _solve(self, empty_cells, domain):
+        if not empty_cells:
             return True
-        min = self.mrv(domains)
-
-        for i in domains[min]:
-                self.board[min[0]][min[1]] = i
-                self.empty_cell.remove((min[0], min[1]))
-
-                if self.solve():
-                    return True
-                self.board[min[0]][min[1]] = 0
         
-        self.empty_cell.append((min[0], min[1]))
-        return False 
+        # Get the cell with the least amount of possible values
+        cell = self.mrv(empty_cells, domain)
+
+        # Get the all possible values for current cell
+        values = domain[cell]
+
+        # Loop through all possible values
+        for value in values:
+            # Insert the value to the cell
+            self.board[cell[0]][cell[1]] = value
+
+            # Create new_empty_cells
+            new_empty_cells = empty_cells.copy()
+            new_empty_cells.remove(cell)
+
+            # Call forward checking with the new empty cells to get the new domain
+            new_domain = self.forward_checking(new_empty_cells, domain)
+
+            # Check whether all the empty cells in the new_empty_cells list still have possible values in the new_domain
+            if all(len(new_domain[cell]) > 0 for cell in new_empty_cells):
+                result = self._solve(new_empty_cells, new_domain)
+                if result:
+                    return True
+
+        # Backtrack
+        return False
     
     def print_board(self):
         # Print horizontal line
