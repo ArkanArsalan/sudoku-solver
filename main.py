e281dd824360cb79b12f3e3c243e63ea0e866df6
Arkan Arsalan <arkan.arsalan.amanullah@gmail.com>
2023-11-06 11:41:09 +0700

    Update GUI

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-from sudoku_solver1 import SudokuSolver1
-from sudoku_solver2 import SudokuSolver2
+from sudokuGUI import SudokuGUI
 from initial_board import open_file
-import copy
 
 def main():
     board = open_file()
-    copy_board = open_file()
-
-
-    print("Solving using backtracking")
-    s1 = SudokuSolver1(board)
-    s1.solve()
-    s1.print_board()
-    print(f"Total recursive call: {s1.count}")
-
-    print("\nSolving using bactracking, forward checking, and MRV")
-    s2 = SudokuSolver2(copy_board)
-    s2.solve()
-    s2.print_board()
-    print(f"Total recursive call: {s2.count}")
-
+    sudoku = SudokuGUI(board)
+    sudoku.window.mainloop()
 
 if __name__ == "__main__":
     main()
--- sudokuGUI.py
+++ sudokuGUI.py
@@ -1,14 +1,22 @@
 import tkinter as tk
 from sudoku_solver1 import SudokuSolver1
+from sudoku_solver2 import SudokuSolver2
+import copy
 
 class SudokuGUI:
     def __init__(self, board):
         self.board = board
+        self.init_board =  copy.deepcopy(self.board)
         self.window = tk.Tk()
         self.window.title("Sudoku solver")
         self.create_board()
-        self.create_solve_button()
-        self.sudoku = SudokuSolver1(self.board)
+        self.sudoku1 = None
+        self.sudoku2 = None
+        self.open_sudoku_solver()
+
+    def open_sudoku_solver(self):
+        self.sudoku1 = SudokuSolver1(self.board)
+        self.sudoku2 = SudokuSolver2(self.board)
 
     def create_board(self):
         for i in range(9):
@@ -26,13 +34,35 @@ class SudokuGUI:
             self.window.grid_rowconfigure(i, weight=1)
             self.window.grid_columnconfigure(i, weight=1)
 
-    def create_solve_button(self):
-        solve_button = tk.Button(self.window, text="Solve", command=self.solve_sudoku)
-        solve_button.grid(row=9, columnspan=9, pady=10)
+        # Create three buttons below the board
+        button1 = tk.Button(self.window, text="Method 1", command=self.solve_sudoku_method1)
+        button2 = tk.Button(self.window, text="method 2", command=self.solve_sudoku_method2)
+        button3 = tk.Button(self.window, text="Reset", command=self.reset)
+        
+        # Place the buttons in the grid
+        button1.grid(row=9, column=0, padx=10, pady=10, columnspan=3)
+        button2.grid(row=9, column=3, padx=10, pady=10, columnspan=3)
+        button3.grid(row=9, column=6, padx=10, pady=10, columnspan=3)
 
-    def solve_sudoku(self):
-        if self.sudoku.solve():
-            self.update_board()
+
+    def solve_sudoku_method1(self):
+        print("\nSolving using backtracking")
+        self.sudoku1.solve()
+        self.sudoku1.print_board()
+        self.update_board()
+        print(f"Total recursive call: {self.sudoku1.count}")
+
+    def solve_sudoku_method2(self):
+        print("\nSolving using bactracking, forward checking, and MRV")
+        self.sudoku2.solve()
+        self.sudoku2.print_board()
+        self.update_board()
+        print(f"Total recursive call: {self.sudoku2.count}")
+
+    def reset(self):
+        self.board = copy.deepcopy(self.init_board)
+        self.open_sudoku_solver()
+        self.update_board()
 
     def update_board(self):
         for i in range(9):
